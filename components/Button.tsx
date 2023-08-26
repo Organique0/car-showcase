9fc8a616fe64e9c7a368753b26204f2eec12a076
Organique0 <grabnar.luka@gmail.com>
2023-08-27 00:29:46 +0200

    rapid api, car cards, brand combobox

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,12 +5,22 @@ import { ButtonProps } from '@/types';
 
 
 
-const Button = ({ title, styles, handleClick, btnType }: ButtonProps) => {
+const Button = ({ title, ContainerStyles, textStyles, rightIcon, handleClick, btnType, isDisabled }: ButtonProps) => {
     return (
-        <button disabled={false} type={btnType || "button"} className={`custom-btn ${styles}`} onClick={handleClick}>
-            <span className={`flex-1`}>
+        <button disabled={false} type={btnType || "button"} className={`custom-btn ${ContainerStyles}`} onClick={handleClick}>
+            <span className={`flex-1 ${textStyles}`}>
                 {title}
             </span>
+            {rightIcon && (
+                <div className='relative w-6 h-6'>
+                    <Image
+                        src={rightIcon}
+                        alt="right-icon"
+                        fill
+                        className='object-contain'
+                    />
+                </div>
+            )}
         </button>
     )
 }
